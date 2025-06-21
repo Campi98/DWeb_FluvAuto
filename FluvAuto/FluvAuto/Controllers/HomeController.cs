179e3caec944d7ae16b40c5680b4be979af07aab
Campi98 <joaoccampos98@gmail.com>
2025-06-21 22:52:14 +0100

    Criada user page; referência ao fullcalendar; dias com marcação ficam highlighted

--- FluvAuto/FluvAuto/Controllers/HomeController.cs
+++ FluvAuto/FluvAuto/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FluvAuto.Models;
+using FluvAuto.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FluvAuto.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -30,6 +34,15 @@ public class HomeController : Controller
 
     public IActionResult UserPage()
     {
+        var username = User.Identity?.Name;
+        var clienteId = _context.Clientes.Where(c => c.UserName == username).Select(c => c.UtilizadorId).FirstOrDefault();
+        var datasMarcacoes = _context.Marcacoes
+            .Where(m => m.Viatura != null && m.Viatura.ClienteFK == clienteId)
+            .Include(m => m.Viatura)
+            .Select(m => m.DataMarcacaoFeita.Date)
+            .ToList();
+        var datasMarcacoesStr = datasMarcacoes.Select(d => d.ToString("yyyy-MM-dd")).ToList();
+        ViewBag.DatasMarcacoes = datasMarcacoesStr;
         return View();
     }
 
