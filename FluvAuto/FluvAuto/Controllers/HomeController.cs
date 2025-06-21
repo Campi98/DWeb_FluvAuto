495ade7fd4d216a3747d9456615070f670c3868f
Campi98 <joaoccampos98@gmail.com>
2025-06-21 22:21:23 +0100

    Adicionada user page

--- FluvAuto/FluvAuto/Controllers/HomeController.cs
+++ FluvAuto/FluvAuto/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ public class HomeController : Controller
         return View("Privacy");
     }
 
+    public IActionResult UserPage()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
