473aaca3dac2d177cf8ab3dd274cc2de59bafd7a
Campi98 <joaoccampos98@gmail.com>
2025-06-21 12:36:13 +0100

    Cor principal da App; Footer; "Sobre a App"

--- FluvAuto/FluvAuto/Controllers/HomeController.cs
+++ FluvAuto/FluvAuto/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View("Privacy");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
